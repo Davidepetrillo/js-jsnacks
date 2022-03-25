bbd5dc78d22ffe5f91d3dcf141c2057af24aa97a
davidepetrillo <davide96petrillo@gmail.com>
2022-03-25 18:04:08 +0100

    commit 7 - terminato l'esercizio 3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,8 +1,15 @@
-
+let arrayVuoto = [];
 for(let i=0; i<10; i++){
-    let richiesta = prompt("Inserisci un numero");
-    let risposta = richiesta++;
-    console.log(risposta);
+    let richiesta = parseInt(prompt("Inserisci un numero"));
+    arrayVuoto.push(richiesta);
 }
+document.getElementById("primaRiga").innerHTML = arrayVuoto;
+let sum = 0;
+for (let i = 0; i < arrayVuoto.length; i++) {
+    sum += arrayVuoto[i];
+}
+document.getElementById("secondaRiga").innerHTML = `La somma dei numeri all'interno dell'array è ${sum}`;
+
+
 
 // Da completare domani
